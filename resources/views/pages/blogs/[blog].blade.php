7365a86a0882286559bae464a6f33a6558e4fc66
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-08-24 17:34:57 +0630

    laravel folio course

---
+++ resources/views/pages/blogs/[blog].blade.php
@@ -0,0 +1,38 @@
+<?php
+ 
+use function Laravel\Folio\{middleware};
+use App\Http\Middleware\AuthMiddleware;
+middleware(AuthMiddleware::class);
+?>
+
+@php
+use Illuminate\Support\Facades\Gate;
+
+auth()->loginUsingId(1);
+if(Gate::denies('view',$blog)) {
+abort(403);
+}
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+    <meta
+        http-equiv="X-UA-Compatible"
+        content="ie=edge"
+    >
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>{{$blog->title}}</h1>
+    <h3>Author - {{$blog->author->name}}</h3>
+</body>
+
+</html>
